c3d98fef0851ca40c9499798a8f8e2edab2179f6
iambrj <joshibharathiramana@gmail.com>
2019-03-03 20:25:09 +0530

    added newref

--- cathy.c
+++ cathy.c
@@ -110,6 +110,20 @@ struct ast *newcall(struct symbol *s, struct ast *l)
 	return (struct ast *)a;
 }
 
+struct ast *newref(struct symbol *s)
+{
+	struct symref *a = malloc(sizeof(struct symref));
+
+	if(!a)
+	{
+		yyerror("Out of space!");
+		exit(0);
+	}
+	a->nodetype = 'N';
+	a->s = s;
+	return (struct ast *)a;
+}
+
 void yyerror(char *s, ...)
 {
 	va_list ap;
